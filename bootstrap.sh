562608405a5baf682830498c02e743fe8c9d9748
Hubert Wong <hubert@hubert.io>
2014-03-19 18:55:03 -0700

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-ln -s .gitconfig ~/.gitconfig
-ln -s .gitignore ~/.gitignore
-ln -s .screenrc ~/.screenrc
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
+cd ~
+rm -r ~/.gitconfig ~/.gitignore ~/.screenrc ~/.vim ~/.vimrc
+ln -s dotfiles/.gitconfig ~/.gitconfig
+ln -s dotfiles/.gitignore ~/.gitignore
+ln -s dotfiles/.screenrc ~/.screenrc
+ln -s dotfiles/.vim ~/.vim
+ln -s dotfiles/.vimrc ~/.vimrc
+cd -
 
