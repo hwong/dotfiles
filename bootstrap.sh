935a63797460d0f6519180db746b6d302ad96098
Hubert Wong <hubert@hubert.io>
2014-07-13 04:27:16 -0700

    Refactor

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -ex
 
-cd ~
 rm -r ~/.gitconfig ~/.gitignore ~/.screenrc ~/.vim ~/.vimrc || true
-ln -s dotfiles/.gitconfig ~/.gitconfig
-ln -s dotfiles/.gitignore ~/.gitignore
-ln -s dotfiles/.screenrc ~/.screenrc
-ln -s dotfiles/.vim ~/.vim
-ln -s dotfiles/.vimrc ~/.vimrc
-cd -
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.screenrc ~/.screenrc
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 
